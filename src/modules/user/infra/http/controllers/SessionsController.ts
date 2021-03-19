3043558467163a43e3e61e709e1b1112000fe8db
fernangb <gfsouza@poli.ufrj.br>
2021-03-19 19:59:04 -0300

    Ajuste na mensagem de erro ao fazer login

--- src/modules/user/infra/http/controllers/SessionsController.ts
+++ src/modules/user/infra/http/controllers/SessionsController.ts
@@ -5,18 +5,22 @@ import AuthenticateUserService from '../../../services/AuthenticateUserService';
 
 export default class SessionsController {
   public async create(request: Request, response: Response): Promise<Response> {
-    const { email, password } = request.body;
+    try {
+      const { email, password } = request.body;
 
-    const autenthicatedUser = container.resolve(AuthenticateUserService);
+      const autenthicatedUser = container.resolve(AuthenticateUserService);
 
-    const { user, token } = await autenthicatedUser.execute({
-      email,
-      password,
-    });
+      const { user, token } = await autenthicatedUser.execute({
+        email,
+        password,
+      });
 
-    return response.json({
-      user: classToClass(user),
-      token,
-    });
+      return response.json({
+        user: classToClass(user),
+        token,
+      });
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
