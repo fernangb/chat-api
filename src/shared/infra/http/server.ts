700cb6df176e033b036f37dda52f5749efd5d32d
fernangb <gfsouza@poli.ufrj.br>
2021-03-21 15:22:05 -0300

    Coonectando socket io

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,15 +1,38 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import 'reflect-metadata';
 
-import express from 'express';
-
+import express, { Request, Response } from 'express';
+import path from 'path';
+// import io from 'socket.io';
 import routes from './routes';
 import '../typeorm';
 import '../../container';
 
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+app.get('/', (request: Request, response: Response) => {
+  response.sendFile(`${__dirname}/client/index.html`);
+});
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query;
+  console.log(socket.id);
+
+  socket.on('chat message', msg => {
+    console.log('user: ', msg);
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3333, () => {
   console.log('\n--- Server started on port 3333 ---\n');
 });
